b1f197680559cf7ce6a73789d0f39d544a7434ba
Manoj <manojmkt127@gmail.com>
2021-01-27 09:28:28 -0500

    Assignment 1 - Slot Machine App created

---
+++ Assignment1/SlotMachineApp/SlotMachineApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlotMachineApp
+//
+//  Created by Manoj on 2021-01-27.
+//  Copyright © 2021 Manoj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
